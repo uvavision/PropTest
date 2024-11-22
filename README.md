9f2bc7075759efe6dbf0cfadb99242fba63b03b1
Jaywon Koo <34685762+JaywonKoo17@users.noreply.github.com>
2024-11-21 21:37:18 -0600

    Release

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# PropTest
-The code coming soon! 
+# PropTest: Automatic Property Testing for Improved Visual Programming
+
+This is the code for the paper [PropTest: Automatic Property Testing for Improved Visual Programming](https://jaywonkoo17.github.io/PropTest/) 
+
+### [Paper (EMNLP 2024 Findings)](https://arxiv.org/pdf/2403.16921) | [Project Page](https://jaywonkoo17.github.io/PropTest/)
+
+# Environmnet
+
+---
+Clone recursively:
+```bash
+git clone --recurse-submodules https://github.com/uvavision/PropTest.git
+```
+
+After cloning:
+```bash
+cd PropTest
+export PATH=/usr/local/cuda/bin:$PATH
+bash setup.sh  # This may take a while. Make sure the vipergpt environment is active
+cd GLIP
+python setup.py clean --all build develop --user
+cd ..
+echo YOUR_OPENAI_API_KEY_HERE > api.key
+```
+This code was built on top of [ViperGPT](https://github.com/cvlab-columbia/viper). We follow the same installation steps as ViperGPT. For detailed installation, please refer to the [ViperGPT repository](https://github.com/cvlab-columbia/viper).
+
+You need to download two pretrained models and store it in ```./pretrained_models```. 
+You can use ```download_models.sh``` to download the models.
+
+# Running the Code
+
+---
+
+The code can be run using the following command:
+    
+```
+CONFIG_NAMES=your_config_name python main_batch.py
+```
+```CONFIG_NAMES``` is an environment variable that specifies the configuration files to use.
+
+# Citation
+
+---
+Please cite our paper if you find our method or code useful:
+```
+@article{koo2024proptest,
+      title={PropTest: Automatic Property Testing for Improved Visual Programming}, 
+      author={Jaywon Koo and Ziyan Yang and Paola Cascante-Bonilla and Baishakhi Ray and Vicente Ordonez},
+      journal={arXiv preprint arXiv:2403.16921},
+      year={2024}
+}
+```
---
+++ datasets/__init__.py
@@ -0,0 +1,84 @@
+import torch
+
+from torchvision import transforms
+
+
+# ----------------------------- General for all datasets ----------------------------- #
+def get_dataset(config_dataset):
+    dataset_name = config_dataset.dataset_name
+
+    if dataset_name == 'RefCOCO':
+        from datasets.refcoco import RefCOCODataset
+        dataset = RefCOCODataset(**config_dataset,
+                                 image_transforms=transforms.Compose([transforms.ToTensor()]))
+
+    elif dataset_name == 'AOKVQA':
+        from datasets.aokvqa import AOKVQADataset
+        dataset = AOKVQADataset(**config_dataset,
+                                image_transforms=transforms.Compose([transforms.ToTensor()]))
+
+    elif dataset_name == 'MyDataset':
+        from datasets.my_dataset import MyDataset
+        dataset = MyDataset(**config_dataset)
+    else:
+        raise ValueError(f"Unknown dataset {dataset_name}")
+    return dataset
+
+def general_postprocessing(prediction):
+    try:
+        if type(prediction).__name__ == 'ImagePatch':
+            prediction = prediction.classify_object()
+
+        if isinstance(prediction, list):
+            prediction = prediction[0] if len(prediction) > 0 else "no"
+
+        if isinstance(prediction, torch.Tensor):
+            prediction = prediction.item()
+        if prediction is None:
+            prediction = "no"
+        if isinstance(prediction, bool):
+            if prediction:
+                prediction = "yes"
+            else:
+                prediction = "no"
+        elif isinstance(prediction, int):
+            prediction = str(prediction)
+            print("No answer is a number, so this will be wrong")
+    except:
+        prediction = str(prediction)
+
+    prediction = str(prediction)
+
+    prediction = prediction.replace('\n', ' ')
+    prediction = prediction.replace('\t', ' ')
+    prediction = prediction.strip()
+    prediction = prediction.lower()
+
+    if prediction == 'true':
+        prediction = 'yes'
+    elif prediction == 'false':
+        prediction = 'no'
+    return prediction
+
+
+def accuracy(prediction, ground_truth, *args):
+    """
+    Args:
+        prediction (list): List of predicted answers.
+        ground_truth (list): List of ground truth answers.
+    Returns:
+        score (float): Score of the prediction.
+    """
+    if len(prediction) == 0:  # if no prediction, return 0
+        return 0
+    assert len(prediction) == len(ground_truth)
+    pred_gt_filtered = [(pred, gt) for pred, gt in zip(prediction, ground_truth) if gt != '']
+    score = 0
+    for p, g in pred_gt_filtered:
+        if general_postprocessing(p) == g:
+            score += 1
+    return score / len(pred_gt_filtered)
+
+
+def all_answers_from_dict(dct):
+    return [x["answer"] for x in dct]
